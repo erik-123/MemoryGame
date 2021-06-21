c4247fd052d8fc1f96a83a47966746363a09ffbf
erik-123 <erik.sondell@outlook.com>
2021-06-21 13:55:38 +0200

    image size converter

--- app/src/main/java/com/erik123/memorygame/CreateActivity.kt
+++ app/src/main/java/com/erik123/memorygame/CreateActivity.kt
@@ -4,10 +4,17 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
 import android.nfc.Tag
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
 import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
@@ -16,9 +23,11 @@ import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.erik123.memorygame.models.BoardSize
+import com.erik123.memorygame.utils.BitmapScaler
 import com.erik123.memorygame.utils.EXTRA_BOARD_SIZE
 import com.erik123.memorygame.utils.isPermissionGranted
 import com.erik123.memorygame.utils.requestPermission
+import java.io.ByteArrayOutputStream
 import java.net.URI
 
 class CreateActivity : AppCompatActivity() {
@@ -28,6 +37,8 @@ class CreateActivity : AppCompatActivity() {
        private const val PICK_PHOTO_CODE = 655
        private const val READ_EXTERNAL_PHOTOS_CODE = 248
        private const val READ_PHOTOS_PERMISSION = android.Manifest.permission.READ_EXTERNAL_STORAGE
+       private const val MIN_GAME_CHAR_LENGTH = 3
+       private const val MAX_GAME_CHAR_LENGTH = 14
    }
 
 
@@ -56,6 +67,19 @@ class CreateActivity : AppCompatActivity() {
         numImagesRequired = boardSize.getNumPairs()
         supportActionBar?.title = "Choose images (0/ $numImagesRequired) "
 
+        btnSave.setOnClickListener{
+            saveDataToFirebase()
+        }
+        etGameName.filters = arrayOf(InputFilter.LengthFilter(MAX_GAME_CHAR_LENGTH))
+        etGameName.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                btnSave.isEnabled = shouldEnableSaveButton()
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
          adapter = ImagePickerAdapter(this, chosenImageUris, boardSize, object: ImagePickerAdapter.ImageClickListener{
             override fun onPlaceholderClicked() {
                 if(isPermissionGranted(this@CreateActivity,READ_PHOTOS_PERMISSION))
@@ -77,6 +101,8 @@ class CreateActivity : AppCompatActivity() {
 
 
     }
+
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -136,6 +162,14 @@ class CreateActivity : AppCompatActivity() {
 
     private fun shouldEnableSaveButton(): Boolean {
         //Check if save button should be updated
+        if(chosenImageUris.size != numImagesRequired){
+
+            return false
+        }
+        if(etGameName.text.isEmpty() || etGameName.text.length < MIN_GAME_CHAR_LENGTH)
+        {
+            return false
+        }
         return true
 
     }
@@ -150,5 +184,29 @@ class CreateActivity : AppCompatActivity() {
 
 
         return super.onOptionsItemSelected(item)
+    }
+    private fun saveDataToFirebase() {
+            Log.i(TAG, "Save to Firebase")
+        for((index, photoUri) in chosenImageUris.withIndex())
+        {
+            val imageByteArray = getImageByteArray(photoUri)
+        }
+    }
+
+    private fun getImageByteArray(photoUri: Uri): ByteArray {
+        val originalBitmap = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+        {
+            val source = ImageDecoder.createSource(contentResolver, photoUri)
+            ImageDecoder.decodeBitmap(source)
+        }else{
+            MediaStore.Images.Media.getBitmap(contentResolver, photoUri)
+        }
+        Log.i(TAG, "Original width ${originalBitmap.width} and height ${originalBitmap.height}")
+        val scaledBitmap = BitmapScaler.scaleToFitHeight(originalBitmap, 250)
+        Log.i(TAG, "Scaled width ${scaledBitmap.width} and height ${scaledBitmap.height}")
+
+        val byteOutputStream = ByteArrayOutputStream()
+        scaledBitmap.compress(Bitmap.CompressFormat.JPEG,60, byteOutputStream)
+        return byteOutputStream.toByteArray()
     }
 }
